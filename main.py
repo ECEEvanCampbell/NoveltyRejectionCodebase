b65bf273e19cbff982bb38a481518e7d0f7a71cc
ECEEvanCampbell <evan.campbell1@unb.ca>
2021-07-08 20:29:40 -0300

    Added .npy conversion

--- main.py
+++ main.py
@@ -1,17 +1,53 @@
 import torch
 import numpy as np
 import os
+import math
+
+
+def compute_center_loss(features, centers, targets):
+    features = features.view(features.size(0), -1)
+    target_centers = centers[targets]
+    criterion = torch.nn.MSELoss()
+    center_loss = criterion(features, target_centers)
+    return center_loss
+
+def make_npy(subject_id, dataset_characteristics, base_dir="Data/Raw_Data"):
+    # Inside of dataset folder, get list of all files associated with the subject of subject_id
+    (num_subjects, num_channels, num_reps, num_motions, winsize, wininc, sampling_frequency) = dataset_characteristics
+    subj_path = os.listdir(base_dir+'/S' + str(subject_id + 1))
+    training_data = []
+    # For this list:
+    for f in subj_path:
+        # Get the identifiers in the filename
+        path = os.path.join(base_dir,"S"+ str(subject_id+1),f)
+        class_num = int(f.split('_')[1][1:])
+        rep_num   = int(f.split('_')[3][1])
+
+        # load the file
+        data = np.genfromtxt(path,delimiter=',')
+        
+        num_windows = math.floor((data.shape[0]-winsize)/wininc)
+
+        st=0
+        ed=int(st+winsize * sampling_frequency / 1000)
+        for w in range(num_windows):
+            training_data.append([subject_id,class_num-1, rep_num,w,data[st:ed,:].transpose()])
+            st = int(st+wininc * sampling_frequency / 1000)
+            ed = int(ed+wininc * sampling_frequency / 1000)
+
+    np.random.shuffle(training_data)
+    np.save("Data/S"+str(subject_id), training_data)
 
 def main():
 
     # Start by defining some dataset details:
-    num_subjects       = 40
+    num_subjects       = 9 # Full dataset has 40, testing with first 9
     num_reps           = 6 # Rep 0 has 0 windows, there is only 6 reps. For some reason, subject 1 has rep 0 elements in the .mats
     num_motions        = 49
     num_channels       = 48
     sampling_frequency = 1000 # This is assumed, check later.
-    winsize            = 250 # samples
-    wininc             = 100
+    winsize            = 250
+    wininc             = 150
     dataset_characteristics = (num_subjects, num_channels, num_reps, num_motions, winsize, wininc, sampling_frequency)
 
     # Data Division parameters (which classes are used to train CNN/AE models)
@@ -46,9 +82,11 @@ def main():
             make_npy(s_train, dataset_characteristics)
         
         # TODO: make this class
-        CNN_data = EMGData(s_train, chosen_class_labels = CNN_classes)
+        #CNN_data = EMGData(s_train, chosen_class_labels = CNN_classes, channel_shape = [6,8], features = ['MAV','RMS','WL'])
 
 
+#Conv - 2 conv, 1 flatten, 2 linear, softmax
+#centerloss + MSEloss
 
 
 
